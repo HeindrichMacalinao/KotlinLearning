3cad14a727322b25d9804fdc87ecbcb25199ad78
Heindrich Himmler Macalinao <heindrich.macalinao@gmail.com>
2026-02-24 01:23:26 +0800

    Print Function

--- src/com/kotlin/basics/PrintFunction.kt
+++ src/com/kotlin/basics/PrintFunction.kt
@@ -4,5 +4,6 @@ fun main () {
     print("Heindrich Macalinao's ")
     print("Kotlin Learning Journey.")
     print(" Goodluck to me!")
-    print(" This looks fun.")
+    print(" This looks fun. ")
+    print("No semicolons needed!")
 }
